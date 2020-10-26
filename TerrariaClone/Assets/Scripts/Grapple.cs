77f491f003248134ace4643620a932d28e1d9cd5
Zeppelin Games <zeppelingames3@gmail.com>
2020-10-27 10:58:07 +1100

    Added grappling hook

---
+++ TerrariaClone/Assets/Scripts/Grapple.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    public Camera cam;
+    public LineRenderer lr;
+    public LayerMask grappleMask;
+    public float moveSpeed = 2;
+    public float grappleLength = 5;
+    [Min(1)]
+    public int maxPoints = 3;
+
+    private Rigidbody2D rig;
+    private List<Vector2> points = new List<Vector2>();
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        lr.positionCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, grappleLength, grappleMask);
+            if (hit.collider != null)
+            {
+                Vector2 hitPoint = hit.point;
+                points.Add(hitPoint);
+
+                if (points.Count > maxPoints)
+                {
+                    points.RemoveAt(0);
+                }   
+            }
+        }
+
+        if (points.Count > 0)
+        {
+            Vector2 moveTo = centriod(points.ToArray());
+            rig.MovePosition(Vector2.MoveTowards((Vector2)transform.position, moveTo, Time.deltaTime * moveSpeed));
+
+            lr.positionCount = 0;
+            lr.positionCount = points.Count * 2;
+            for (int n = 0, j=0; n < points.Count * 2; n+=2,j++)
+            {
+                lr.SetPosition(n, transform.position);
+                lr.SetPosition(n+1, points[j]);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Detatch();
+        }
+    }
+
+    public void Detatch()
+    {
+        lr.positionCount = 0;
+        points.Clear();
+    }
+
+    Vector2 centriod(Vector2[] points)
+    {
+        Vector2 center = Vector2.zero;
+        foreach (Vector2 point in points)
+        {
+            center += point;
+        }
+        center /= points.Length;
+        return center;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction =  (mousePos - (Vector2)transform.position).normalized;
+
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position +  direction);
+
+        foreach(Vector2 point in points)
+        {
+            Gizmos.DrawLine(transform.position, point);
+        }
+    }
+}
---
+++ TerrariaClone/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Setup")]
+    public float moveSpeed = 5;
+
+    [Header("Jump Setup")]
+    public float jumpForce = 5;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius=0.1f;
+    public LayerMask groundLayer;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    public float coyoteTime=0.1f;
+    float lastTimeGrounded;
+    bool isGrounded;
+
+    private Rigidbody2D rig;
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rig.velocity = new Vector2(h * moveSpeed, rig.velocity.y);
+
+        CheckIfGrounded();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded || Time.time - lastTimeGrounded <= coyoteTime)
+            {
+                Jump();
+            }
+        }
+    }
+
+    void Jump()
+    {
+        rig.velocity = new Vector2(rig.velocity.x, jumpForce);
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (colliders != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+}
